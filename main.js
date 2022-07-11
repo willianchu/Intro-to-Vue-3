3c9e1b41a40e066ec5ea5559aca79794439d2aaa
willianchu <willianchu@hotmail.com>
2022-07-11 10:47:33 -0700

    Add images and link url

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const app = Vue.createApp({
   data() {
     return {
       product: 'F150-Lightning',
-      description: "The best eletric truck in the world"
+      description: "The best electric truck in the world",
+      image: './assets/images/F150Blue.jpg',
+      url: 'https://www.ford.ca/trucks/f150/f150-lightning/'
     }
   }
 })
