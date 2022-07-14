85471d47ddeb77687ccbff7671a1b851e58b309a
willianchu <willianchu@hotmail.com>
2022-07-13 17:01:04 -0700

    components

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -1,98 +1,82 @@
 app.component('product-display', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
   template: 
-    /*html*/ 
-    `<div class="product-display">
-      <div class="product-container">
-        <div class="product-image">
-          <img v-bind:src="image">
-        </div>
-        <div class="product-info">
-          <h1>{{ title }}</h1>
-  
-          <p v-if="inStock">In Stock</p>
-          <p v-else>Out of Stock</p>
-  
-          <div 
-            v-for="(variant, index) in variants" 
-            :key="variant.id" 
-            @mouseover="updateVariant(index)" 
-            class="color-circle" 
-            :style="{ backgroundColor: variant.color }">
-          </div>
-          
-          <button 
-            class="button" 
-            :class="{ disabledButton: !inStock }" 
-            :disabled="!inStock" 
-            v-on:click="addToCart">
-            Add to Cart
-          </button>
-        </div>
+  /*html*/
+  `<div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <img v-bind:src="image">
       </div>
-    </div>`,
-    data() {
-      return {
-        cart: 0,
-        product: 'F150-Lightning',
-        brand: 'Ford',
-        description: "The best electric truck in the world",
-        details: ['signature front lightning', "360° camera", "Pro Power Onboard 9.6Kw"],
-        variants: [
-          { id: 2022, color: 'red', image: './assets/images/F150Red.jpg', quantity: 0 },
-          { id: 2023, color: 'blue', image: './assets/images/F150Blue.jpg', quantity: 11 },
-        ],
-        models: [
-          { id: 20231, model: 'XLT'},
-          { id: 20232, model: 'Lariat'},
-          { id: 20233, model: 'Platinum'}
-        ],
-        url: 'https://www.ford.ca/trucks/f150/f150-lightning/',
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+
+        <p>Shipping: {{ shipping }}</p>
         
-        onSale: true,
+        <!-- solution -->
+        <product-details :details="details"></product-details>
+        <!-- solution -->
+
+        <div 
+          v-for="(variant, index) in variants" 
+          :key="variant.id" 
+          @mouseover="updateVariant(index)" 
+          class="color-circle" 
+          :style="{ backgroundColor: variant.color }">
+        </div>
+        
+        <button 
+          class="button" 
+          :class="{ disabledButton: !inStock }" 
+          :disabled="!inStock" 
+          v-on:click="addToCart">
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  </div>`,
+  data() {
+    return {
+        product: 'F150 Lightning',
+        brand: 'Ford',
         selectedVariant: 0,
-      }
-    },
-    computed: {
-      title() {
-        return this.brand + ' ' + this.product + ' ' + (this.inStock? 'In Stock': 'Sold Out')
-      },
-      inStock() {
-        return this.variants[this.selectedVariant].quantity > 0
-      },
-      image() {
-        return this.variants[this.selectedVariant].image
-      },
-      navBgColor() {
-        return this.selectedVariant === 0 ? 'red' : 'blue'
-      },
-    },
-    methods: {
+        details: ['50% cotton', '30% wool', '20% polyester'],
+        variants: [
+          { id: 2234, color: 'red', image: './assets/images/F150Red.jpg', quantity: 12 },
+          { id: 2235, color: 'blue', image: './assets/images/F150Blue.jpg', quantity: 0 },
+        ]
+    }
+  },
+  methods: {
       addToCart() {
-        this.cart += 1
-        this.inventory -= 1
-        if (this.inventory <= 0) {
-          this.inStock = false
-        }
-      },
-      removeFromCart() {
-        if(this.cart > 0) {
-          this.cart -= 1
-          this.inventory += 1
-          if (this.inventory > 0) {
-            this.inStock = true
-          }
-        }
-      },
-      updateImage(variantImage) {
-        this.image = variantImage
-        if (variantImage === './assets/images/F150Red.jpg') {
-          this.navBgColor = 'red'
-        } else {
-          this.navBgColor = 'blue'
-        }
+          this.cart += 1
       },
       updateVariant(index) {
-        this.selectedVariant = index
+          this.selectedVariant = index
+      }
+  },
+  computed: {
+      title() {
+          return this.brand + ' ' + this.product
       },
-    }
+      image() {
+          return this.variants[this.selectedVariant].image
+      },
+      inStock() {
+          return this.variants[this.selectedVariant].quantity
+      },
+      shipping() {
+        if (this.premium) {
+          return 'Free'
+        }
+        return 2.99
+      }
+  }
 })
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const app = Vue.createApp({
   data() {
-    return {
-      cart: 0,
-      premium: true,
-    }
+      return {
+          cart: 0,
+          premium: true
+      }
   },
   methods: {}
 })
